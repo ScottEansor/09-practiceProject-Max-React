3e3d5941fcdd59ceb9344287ae13382ed7c58bb4
scottyTwoskip <scottyeansor@gmail.com>
2024-12-13 15:24:15 -0700

    project instead of projects on projectView

--- src/components/ProjectView.jsx
+++ src/components/ProjectView.jsx
@@ -1,7 +1,7 @@
 // project title
 //date created
 //description
-export default function ProjectView({ projects }) {
+export default function ProjectView({ project }) {
   return (
     <div className="project-view-container">
       <h1>{project.name}</h1>
